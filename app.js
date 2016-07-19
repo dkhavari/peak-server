373a68b4d35e532d4cd0d825e8a5cd5f8a5c4ee1
dkhavari@gmail.com <dkhavari@gmail.com>
2016-07-19 14:18:12 -0700

    Wrote basic server.

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+/* Set up the app preferences. */
+app.use( require('body-parser').json() );
+
+/* DB variable. */
+var db;
+
+/* Connect to MongoDB. */
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb://peak-user:littlebylittle@ds023485.mlab.com:23485/messages', (err, database) => {
+        if (err) return console.log(err)
+        db = database
+        app.listen(3000, () => {
+                console.log('Peak server is now listening on port 3000...')
+        })
+})
+
+/* Create some routes. */
+app.get('/sms', (req, res) => {
+  console.log('We\'ve received a text: ', req.body);
+})
